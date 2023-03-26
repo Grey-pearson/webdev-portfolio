5c2600386aa11d0974082d62f781ea849d533ac4
Grey P <greytpearson@gmail.com>
2023-03-26 14:46:06 -0600

    added form submition so only colors and imgs left

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 const styles = {
   form: {
@@ -21,23 +22,51 @@ export default function Contact() {
 
   // set logic to get send the form idk how to do that lol
 
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const submitForm = (event) => {
+    event.preventDefault()
+    if (!inputs.userName) {
+      alert('missing username')
+      return
+    }
+    if (!inputs.userEmail) {
+      alert('missing email')
+      return
+    }
+    if (!inputs.userMessage) {
+      alert('missing message')
+      return
+    }
+
+    // send them to myself?? idk
+
+    alert('Message Sent')
+  }
+
   return (
     <div style={styles.center}>
       <h1>Contact Page</h1>
       <p>contact me by submitting this form and ill get back to you at the email you leave me</p>
-      <form style={styles.form}>
+      <form style={styles.form} onSubmit={submitForm}>
         {/* name, email, message to me, all fields is req */}
         <div className="mb-3">
           <label for="userName" className="form-label">Your Name</label>
-          <input type="text" className="form-control" id="userName"></input>
+          <input type="text" className="form-control" name="userName" value={inputs.userName} onChange={handleChange}></input>
         </div>
         <div className="mb-3">
           <label for="userEmail" className="form-label">Your Email</label>
-          <input type="email" className="form-control" id="userEmail"></input>
+          <input type="email" className="form-control" name="userEmail" value={inputs.userEmail} onChange={handleChange}></input>
         </div>
         <div className="mb-3">
           <label for="userMessage" className="form-label">Message for Me</label>
-          <input type="text" className="form-control" id="userMessage"></input>
+          <input type="text" className="form-control" name="userMessage" value={inputs.userMessage} onChange={handleChange}></input>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
